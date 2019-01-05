e8a4a14ace3a70ebf5ee59158f1da7581f79e759
c1ewd <c1ewd@protonmail.com>
2019-01-05 17:09:49 +0700

    Refactor project to package com.server, Listener2 class to Listener, Form class to Server

---
+++ src/com/server/Listener.java
@@ -0,0 +1,108 @@
+package com.server;
+
+import javax.swing.*;
+import java.awt.event.*;
+
+public class Listener extends JDialog {
+    private JPanel contentPane;
+    private JButton buttonListen;
+    private JButton buttonCancel;
+    private JTextField textFieldNick;
+    private JTextField textFieldPort;
+
+    public void setDisableComponents() {
+        textFieldNick.setEnabled(false);
+        textFieldPort.setEnabled(false);
+    }
+
+    public void setEnableComponent() {
+        textFieldNick.setEnabled(true);
+        textFieldPort.setEnabled(true);
+    }
+
+    public Listener(Server form) {
+        setContentPane(contentPane);
+        setModal(true);
+        getRootPane().setDefaultButton(buttonListen);
+        setResizable(false);
+        setSize(260, 140);
+        setLocationRelativeTo((JFrame)form);
+        setTitle("Listener");
+
+        if (form.listen) {
+            System.out.println("Active");
+            buttonListen.setText("Un listen");
+            setDisableComponents();
+            textFieldNick.setText(form.getNickname());
+            textFieldPort.setText(Integer.toString(form.getPort()));
+//                  form.listen = false;
+        } else {
+            System.out.println("Un Active");
+            buttonListen.setText("Listen");
+            setEnableComponent();
+        }
+
+        buttonListen.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                onListen(form);
+            }
+        });
+
+        buttonCancel.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                onCancel();
+            }
+        });
+//
+//        // call onCancel() when cross is clicked
+//        setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+//        addWindowListener(new WindowAdapter() {
+//            public void windowClosing(WindowEvent e) {
+//                onCancel();
+//            }
+//        });
+//
+//        // call onCancel() on ESCAPE
+//        contentPane.registerKeyboardAction(new ActionListener() {
+//            public void actionPerformed(ActionEvent e) {
+//                onCancel();
+//            }
+//        }, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
+    }
+
+    private void onListen(Server form) {
+        //System.out.println("Listen activate");
+        if (!form.listen) {
+            try {
+                System.out.println(textFieldNick.getText());
+                System.out.println(Integer.parseInt(textFieldPort.getText()));
+                form.setNickname(textFieldNick.getText());
+                form.setPort(Integer.parseInt(textFieldPort.getText()));
+                form.clearTextArea();
+                form.setEnableComponents();
+                form.listen = true;
+                dispose();
+            } catch (Exception e) {
+                System.out.println("Check your data in form");
+            }
+        } else {
+            buttonListen.setText("Listen");
+            setEnableComponent();
+            form.setDisableComponents();
+            form.listen = false;
+        }
+    }
+
+    private void onCancel() {
+        dispose();
+    }
+
+    public static void main(String[] args) {
+/*
+        Listener dialog = new Listener();
+        dialog.pack();
+        dialog.setVisible(true);
+        System.exit(0);
+        */
+    }
+}
---
+++ src/com/server/Server.java
@@ -0,0 +1,113 @@
+package com.server;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Server extends JFrame {
+
+    JPanel panel1;
+    private JTextArea textArea1;
+    private JTextField textField1;
+    private JButton buttonSend;
+    private JScrollPane scrollPane;
+    boolean listen;
+
+    public void clearTextArea() {
+        textArea1.setText("");
+    }
+
+    public void setNickname(String nickname) {
+        Nickname = nickname;
+    }
+
+    public String getNickname() {
+        return Nickname;
+    }
+
+    private String Nickname;
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    private int port;
+
+    public void setEnableComponents() {
+        textArea1.setEnabled(true);
+        textField1.setEnabled(true);
+        buttonSend.setEnabled(true);
+    }
+
+    public void setDisableComponents() {
+        textArea1.setEnabled(false);
+        textField1.setEnabled(false);
+        buttonSend.setEnabled(false);
+    }
+
+    public Server(String title) {
+        super(title);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        setPreferredSize(new Dimension(400, 450));
+        setResizable(false);
+        setContentPane(panel1);
+        setDisableComponents();
+
+        scrollPane = new JScrollPane(textArea1);
+        getContentPane().add(scrollPane);
+        JMenuBar menuBar = new JMenuBar();
+        JMenu fileMenu = new JMenu("File");
+        JMenuItem listenerItem = new JMenuItem("Listener");
+        fileMenu.add(listenerItem);
+        listenerItem.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                Listener listener = new Listener(Server.this);
+                listener.setVisible(true);
+//                Listener2 listener = new Listener2(Form.this);
+//                listener.setVisible(true);
+            }
+        });
+        fileMenu.addSeparator();
+        JMenuItem exitItem = new JMenuItem("Exit");
+        fileMenu.add(exitItem);
+        exitItem.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+        JMenu aboutMenu = new JMenu("About");
+        JMenuItem aboutItem = new JMenuItem("About");
+        aboutMenu.add(aboutItem);
+        aboutItem.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(Server.this,
+                        "Simple Chat Server\n\n© 2019. All right reserved",
+                        "About",
+                        JOptionPane.INFORMATION_MESSAGE);
+            }
+        });
+        menuBar.add(fileMenu);
+        menuBar.add(aboutMenu);
+        setJMenuBar(menuBar);
+        pack();
+        setLocationByPlatform(true);
+        setLocationRelativeTo(null);
+        textArea1.setEditable(false);
+        buttonSend.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (!textField1.getText().isEmpty()) {
+                    System.out.println(textField1.getText());
+                    textArea1.append(getNickname() + ": " + textField1.getText() + "\n");
+                    textField1.setText("");
+                }
+            }
+        });
+    }
+}
